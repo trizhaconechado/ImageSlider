05d249fc19fa050e02cde959e03eec6bd508deda
surnox <119321167+surnox@users.noreply.github.com>
2023-07-23 08:50:16 +0800

    adding queries

--- app/src/main/java/com/example/imageslider/MainActivity.java
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.imageslider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Connection con;
+    Statement stmt;
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,5 +36,51 @@ public class MainActivity extends AppCompatActivity {
         slideModels.add(new SlideModel(R.drawable.image4, ScaleTypes.FIT));
 
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+    }*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageSlider imageSlider  = findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        try {
+            con = connectionClass();
+            stmt = con.createStatement();
+            String query = "SELECT * FROM Store_Item_List WHERE Item_Category = 'Jackets'";
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String image = "https://c413-2001-4450-4670-4f00-2c06-787d-fa3b-20ba.ap.ngrok.io/Pimages/"
+                        + rs.getString("Item_Image");
+
+                slideModels.add(new SlideModel(image, ScaleTypes.FIT));
+            }
+
+            con.close();
+            imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    @SuppressLint("NewApi")
+    public Connection connectionClass() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://sqlserverrcloset.database.windows.net:1433;DatabaseName=DbRcloset;user=adminRcloset@sqlserverrcloset;password=Password_Rcloset;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(connectionURL);
+        } catch (Exception e) {
+            Log.e("SQL Connection Error: ", e.getMessage());
+        }
+
+        return connection;
+    }
+
 }
